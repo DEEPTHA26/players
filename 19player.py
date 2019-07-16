61af16e0aed2dcb41f2f955ff1c944170cd28d0a
DEEPTHA26 <51949707+DEEPTHA26@users.noreply.github.com>
2019-07-16 17:15:31 +0530

    Update 19player.py

--- 19player.py
+++ 19player.py
@@ -1,13 +1,23 @@
-u,l=map(int,input().split())
-count=0
-for i in range(u,l+1):
-    c=0
-    if i>1:
-        for j in range(2,i):
-            if i%j==0:
-                c=1
+def prime(n):              #this function is used to check the prime or not
+    if n==1:
+        return False
+    elif n==2:
+        return True
+    else:
+        c5=0
+        for i in range(2,n):
+            if n%i==0:
+                c5=1
                 break
-        if c==0:
-            count+=1
-print(count)
-        
+        if c5==0:
+            return True
+        else:
+            return False
+
+inp=int(input())         #get the input
+final=[]
+for h in range(2,inp+1):
+    if prime(h):
+        if inp%h==0:
+            final.append(str(h))
+print(" ".join(final))         #print the value
